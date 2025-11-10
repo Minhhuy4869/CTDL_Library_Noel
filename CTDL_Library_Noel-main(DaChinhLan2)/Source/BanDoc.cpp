31197e01df943eb9672e088fcfa6f90cf2b1e729
nguyenkhactrug <5001103082@student.hcmue.edu.vn>
2025-11-10 21:03:53 +0700

    Add files via upload

---
+++ CTDL_Library_Noel-main(DaChinhLan2)/Source/BanDoc.cpp
@@ -0,0 +1,43 @@
+#include "BanDoc.h"
+
+// getters
+string BanDoc::getMaBanDoc()
+{
+	return strMaBanDoc;
+}
+string BanDoc::getHoTen()
+{
+	return strHoTen;
+}
+string BanDoc::getNgayDangKy()
+{
+	return strNgayDangKy;
+}
+//setters
+void BanDoc::setMaBanDoc(string maBanDoc)
+{
+	strMaBanDoc = maBanDoc;
+}
+void BanDoc::setHoTen(string hoTen)
+{
+	strHoTen = hoTen;
+}
+void BanDoc::setNgayDangKy(string ngayDangKy)
+{
+	strNgayDangKy = ngayDangKy;
+}
+// constructor
+// Khoi tao BanDoc mac dinh
+BanDoc::BanDoc()
+{
+	strMaBanDoc = "";
+	strHoTen = "";
+	strNgayDangKy = "";
+}
+// Khoi tao BanDoc co tham so
+BanDoc::BanDoc(string maBanDoc, string hoTen, string ngayDangKy)
+{
+	strMaBanDoc = maBanDoc;
+	strHoTen = hoTen;
+	strNgayDangKy = ngayDangKy;
+}
---
+++ CTDL_Library_Noel-main(DaChinhLan2)/Source/Brain.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "iostream"
+#include "string"
+#include "sstream"
+#include "ctime"
+#include "windows.h"
+#include "iomanip"
+#include "conio.h"
+
+using namespace std;
+
+//--------------------------------------------------------------------------------------
+// NHÓM HÀM XỬ LÝ GIAO DIỆN CONSOLE
+//--------------------------------------------------------------------------------------
+void setColor(int color);
+void setColor(int background_color, int text_color);
+void loading(int sleep, int n);
+
+//--------------------------------------------------------------------------------------
+// NHÓM HÀM XỬ LÝ NGÀY THÁNG VÀ THỜI GIAN
+//--------------------------------------------------------------------------------------
+long getTimeNow();
+long convertDateToTimestamp(int day, int month, int year, int hour = 0, int min = 0, int sec = 0);
+string getCurrentDate();
+
+//--------------------------------------------------------------------------------------
+// NHÓM HÀM XỬ LÝ CHUỖI VÀ INPUT
+//--------------------------------------------------------------------------------------
+string convertUpperCaseAndDeleteSpace(string x);
+string convertUpperCase(string x);
+string passwordInput(int maxLength);
+bool checkContainsSpacing(string s);
+bool checkIsNumberString(string s);
+
+//--------------------------------------------------------------------------------------
+// NHÓM HÀM MENU
+//--------------------------------------------------------------------------------------
+void menuChucNang();        // Menu Chính (Level 1)
+void dangNhapNhanVien(string& user, string& password); // Form Đăng nhập
+void menuNhanVien();        // Menu Quản lý Thư viện (Level 2)
+void menuAdminThongKe();    // Menu Thống kê (Level 3 - Con của Level 2)
+void menuQuanLySach();      // Menu Quản lý Sách (Level 3 - Con của Level 2)
+void menuQuanLyPhieuMuon(); // Menu Quản lý Phiếu Mượn (Level 3 - Con của Level 2)
