3429cf9aa1f0ff5557c4cc51f7aebcf55819e620
Minh Huy <minhhuydayne@gmail.com>
2025-11-18 17:45:42 +0700

    sửa chuẩn code

--- CTDL_Library_Noel-main(Lan3)/Source/PhieuMuon.cpp
+++ CTDL_Library_Noel-main(Lan3)/Source/PhieuMuon.cpp
@@ -1,36 +1,38 @@
 #include "PhieuMuon.h"
 
 // Constructor
-PhieuMuon::PhieuMuon() {
-    strMaPhieu = "";
-    strMaBanDoc = "";
-    strMaSach = "";
-    strNgayMuon = "";
-    strNgayTra = "";
-    iTinhTrang = 1;
+PhieuMuon::PhieuMuon() 
+{
+    _strMaPhieu = "";
+    _strMaBanDoc = "";
+    _strMaSach = "";
+    _strNgayMuon = "";
+    _strNgayTra = "";
+    _iTinhTrang = 1;
 }
 
-PhieuMuon::PhieuMuon(string maPhieu, string maBanDoc, string maSach, string ngayMuon, string ngayTra, int tinhTrang) {
-    strMaPhieu = maPhieu;
-    strMaBanDoc = maBanDoc;
-    strMaSach = maSach;
-    strNgayMuon = ngayMuon;
-    strNgayTra = ngayTra;
-    iTinhTrang = tinhTrang;
+PhieuMuon::PhieuMuon(string maPhieu, string maBanDoc, string maSach, string ngayMuon, string ngayTra, int tinhTrang) 
+{
+    _strMaPhieu = maPhieu;
+    _strMaBanDoc = maBanDoc;
+    _strMaSach = maSach;
+    _strNgayMuon = ngayMuon;
+    _strNgayTra = ngayTra;
+    _iTinhTrang = tinhTrang;
 }
 
 // Getters
-string PhieuMuon::getMaPhieu() { return strMaPhieu; }
-string PhieuMuon::getMaBanDoc() { return strMaBanDoc; }
-string PhieuMuon::getMaSach() { return strMaSach; }
-string PhieuMuon::getNgayMuon() { return strNgayMuon; }
-string PhieuMuon::getNgayTra() { return strNgayTra; }
-int PhieuMuon::getTinhTrang() { return iTinhTrang; }
+string PhieuMuon::getMaPhieu() { return _strMaPhieu; }
+string PhieuMuon::getMaBanDoc() { return _strMaBanDoc; }
+string PhieuMuon::getMaSach() { return _strMaSach; }
+string PhieuMuon::getNgayMuon() { return _strNgayMuon; }
+string PhieuMuon::getNgayTra() { return _strNgayTra; }
+int PhieuMuon::getTinhTrang() { return _iTinhTrang; }
 
 // Setters
-void PhieuMuon::setMaPhieu(string maPhieu) { strMaPhieu = maPhieu; }
-void PhieuMuon::setMaBanDoc(string maBanDoc) { strMaBanDoc = maBanDoc; }
-void PhieuMuon::setMaSach(string maSach) { strMaSach = maSach; }
-void PhieuMuon::setNgayMuon(string ngayMuon) { strNgayMuon = ngayMuon; }
-void PhieuMuon::setNgayTra(string ngayTra) { strNgayTra = ngayTra; }
-void PhieuMuon::setTinhTrang(int tinhTrang) { iTinhTrang = tinhTrang; }
+void PhieuMuon::setMaPhieu(string maPhieu) { _strMaPhieu = maPhieu; }
+void PhieuMuon::setMaBanDoc(string maBanDoc) { _strMaBanDoc = maBanDoc; }
+void PhieuMuon::setMaSach(string maSach) { _strMaSach = maSach; }
+void PhieuMuon::setNgayMuon(string ngayMuon) { _strNgayMuon = ngayMuon; }
+void PhieuMuon::setNgayTra(string ngayTra) { _strNgayTra = ngayTra; }
+void PhieuMuon::setTinhTrang(int tinhTrang) { _iTinhTrang = tinhTrang; }
