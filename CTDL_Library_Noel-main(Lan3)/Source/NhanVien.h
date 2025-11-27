a4d84a10917895aa9a7de0bbea979684f680eaae
Minh Huy <minhhuydayne@gmail.com>
2025-11-27 18:07:36 +0700

    xóa note

--- CTDL_Library_Noel-main(Lan3)/Source/NhanVien.h
+++ CTDL_Library_Noel-main(Lan3)/Source/NhanVien.h
@@ -7,16 +7,10 @@ private:
 	string _strUser;
 	string _strPassWord;
 public:
-	// getters
 	string getUser();
 	string getPassWord();
-	// setters
 	void setUser(string User);
 	void setPassWord(string PassWord);
-	// constructor
-	// Khoi tao NhanVien mac dinh
 	NhanVien();
-	// Khoi tao NhanVien co tham so
 	NhanVien(string User, string PassWord);
-
 };
