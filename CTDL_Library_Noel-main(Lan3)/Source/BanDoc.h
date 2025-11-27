a6bd1868344092ab2a1ff67a34397cf890e97a8b
Minh Huy <minhhuydayne@gmail.com>
2025-11-27 18:05:42 +0700

    xóa note

--- CTDL_Library_Noel-main(Lan3)/Source/BanDoc.h
+++ CTDL_Library_Noel-main(Lan3)/Source/BanDoc.h
@@ -8,18 +8,13 @@ private:
 	string _strHoTen;
 	string _strNgayDangKy;
 public:
-	// getters
 	string getMaBanDoc();
 	string getHoTen();
 	string getNgayDangKy();
-	//setters
 	void setMaBanDoc(string maBanDoc);
 	void setHoTen(string hoTen);
 	void setNgayDangKy(string ngayDangKy);
-	// constructor
-	// Khoi tao BanDoc mac dinh
 	BanDoc();
-	// Khoi tao BanDoc co tham so
 	BanDoc(string maBanDoc, string hoTen, string ngayDangKy);
-
 };
+
